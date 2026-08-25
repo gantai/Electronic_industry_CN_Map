3c638243559bf8f969497e217cbd2b4cad623092
John Yip <johnyip414@gmail.com>
2026-08-25 17:57:01 -0500

    Revert "补录 《北京工业志·电子志》2001 第三章"
    This reverts commit 7e1ed6a313c9dc0d06b842ac786bbd82f258a8a0.

--- run-beijing.ps1
+++
@@ -1,3 +0,0 @@
-﻿$md = (Get-ChildItem "D:\Archive\转换稿\地方志\*第三章.md").FullName
-$fx = (Get-ChildItem "D:\Archive\转换稿\地方志\*.fixes.tsv").FullName
-python tools\gazetteer\gaz.py book $md --city Beijing --min-mentions 1 --fixes $fx
